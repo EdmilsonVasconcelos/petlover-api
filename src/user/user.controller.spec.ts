b4f4c645408f31803d81b2a86f5b975fa6100740
EdmilsonVasconcelos <edmilson.vasconcelos89@gmail.com>
2024-02-17 16:15:36 -0300

    feat: adding user controllers tests

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -4,6 +4,7 @@ import { User } from './user.entity';
 import { Test } from '@nestjs/testing';
 import { UserIdGuard } from '../guards/user.id.guard';
 import { UserResponseDto } from './dto/user.response.dto';
+import { UserUpsertDto } from './dto/user.upsert.dto';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -47,18 +48,67 @@ describe('UserController', () => {
     return user;
   }
 
-  describe('findAll', () => {
-    it('should return an array of cats', async () => {
-      const users: User[] = [createMockUser()];
+  describe('Get', () => {
+    describe('findAll', () => {
+      it('should return an array of users', async () => {
+        const users: User[] = [createMockUser()];
 
-      const responseExpected: UserResponseDto[] =
-        UserResponseDto.toListDto(users);
+        const responseExpected: UserResponseDto[] =
+          UserResponseDto.toListDto(users);
 
-      service.findAll.mockResolvedValue(users);
+        service.findAll.mockResolvedValue(users);
 
-      const response = await controller.findAll();
+        const response = await controller.findAll();
 
-      expect(response).toStrictEqual(responseExpected);
+        expect(response).toStrictEqual(responseExpected);
+      });
+    });
+
+    describe('findById', () => {
+      it('should return searched user', async () => {
+        const user = createMockUser();
+
+        service.findOne.mockResolvedValue(user);
+
+        const response = await controller.findById('someId');
+
+        expect(response).toStrictEqual(UserResponseDto.toDto(user));
+      });
+    });
+  });
+
+  describe('Post', () => {
+    it('should create a user', async () => {
+      const user = createMockUser();
+
+      service.upsert.mockResolvedValue(user);
+
+      const response = await controller.save({} as UserUpsertDto);
+
+      expect(response).toStrictEqual(UserResponseDto.toDto(user));
+    });
+  });
+
+  describe('Put', () => {
+    it('should update a user', async () => {
+      const user = createMockUser();
+
+      service.upsert.mockResolvedValue(user);
+
+      const response = await controller.update({} as UserUpsertDto);
+
+      expect(response).toStrictEqual(UserResponseDto.toDto(user));
+    });
+  });
+
+  describe('Delete', () => {
+    it('should delete a user', async () => {
+      service.remove.mockResolvedValue();
+
+      await controller.remove('someId');
+
+      expect(service.remove).toHaveBeenCalledWith('someId');
+      expect(service.remove).toHaveBeenCalledTimes(1);
     });
   });
 });
